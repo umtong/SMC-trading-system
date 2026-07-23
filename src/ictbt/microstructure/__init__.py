d2da7461ddf2a2b540bac739663948ed0ad6aad1
umtong <dddgjj03@gmail.com>
2026-07-23 13:34:27 +0900

    Export V0.9.1 dual-clock microstructure API

--- src/ictbt/microstructure/__init__.py
+++ src/ictbt/microstructure/__init__.py
@@ -6,6 +6,19 @@ from .aggtrades import (
     aggregate_trade_flow,
     normalize_aggtrades,
 )
+from .dual_clock import (
+    DualClockFlowDecision,
+    DualClockFlowFeatures,
+    DualClockSceneKind,
+    FrozenDualClockScene,
+    IntervalFlowStats,
+    LOWER_QUANTILE,
+    MEDIAN_QUANTILE,
+    REFERENCE_WINDOWS,
+    UPPER_QUANTILE,
+    evaluate_fixed_dual_clock_flow,
+    required_dual_clock_flow_interval,
+)
 from .funding import FUNDING_COLUMNS, normalize_funding_rates
 from .liquidity_delivery import (
     EVENT_BARS,
@@ -19,10 +32,18 @@ from .liquidity_delivery import (
     evaluate_fixed_flow_reversal,
 )
 from .scene_adapter import AdaptedFlowScene, adapt_authority_to_flow_scene
+from .scene_adapter_v1 import (
+    AdaptedDualClockScene,
+    adapt_authority_to_dual_clock_scene,
+)
 
 __all__ = [
     "AGGTRADE_COLUMNS",
+    "AdaptedDualClockScene",
     "AdaptedFlowScene",
+    "DualClockFlowDecision",
+    "DualClockFlowFeatures",
+    "DualClockSceneKind",
     "EVENT_BARS",
     "EXHAUSTION_QUANTILE",
     "FLOW_COLUMNS",
@@ -30,12 +51,21 @@ __all__ = [
     "FlowReversalDecision",
     "FlowReversalFeatures",
     "FlowSceneKind",
+    "FrozenDualClockScene",
     "FrozenFlowScene",
     "HISTORY_BARS",
+    "IntervalFlowStats",
+    "LOWER_QUANTILE",
+    "MEDIAN_QUANTILE",
+    "REFERENCE_WINDOWS",
     "REVERSAL_QUANTILE",
+    "UPPER_QUANTILE",
+    "adapt_authority_to_dual_clock_scene",
     "adapt_authority_to_flow_scene",
     "aggregate_trade_flow",
+    "evaluate_fixed_dual_clock_flow",
     "evaluate_fixed_flow_reversal",
     "normalize_aggtrades",
     "normalize_funding_rates",
+    "required_dual_clock_flow_interval",
 ]
